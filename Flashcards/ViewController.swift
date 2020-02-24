12cd6449534805348df80a879fb4e627cf9ae92a
Jenina Matela <jlsmatela@gmail.com>
2020-02-23 17:02:42 -0800

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jenina Matela on 2/23/20.
+//  Copyright © 2020 Jenina Matela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
